2fe313c749ca6b82cbc00c0bb25d33f1f1a91ee0
Rasoki <salas.rasoki@gmail.com>
2021-11-22 15:42:37 +0700

    ubah jadi .exe

---
+++ Sesi01/HelloMono/tempCodeRunnerFile.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace HelloMono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Nama Saya Oki");
+        }
+    }
+}
