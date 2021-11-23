0782b1b993e32789eb5180d3da4573783d1269c3
Rasoki <salas.rasoki@gmail.com>
2021-11-23 10:49:43 +0700

    tambah sesi 3

---
+++ Sesi03/Soal6.cs
@@ -0,0 +1,20 @@
+using System;
+
+public class Logika6
+{
+    public static void Main()
+    {
+       // membuat variable
+       int a = 10;
+
+       // loop while
+       while (a < 20) // menhasikan TRUE jika variable a lebih kecil dari 20
+       {
+           Console.WriteLine("Nilai a = " + a);
+            a = a + 1;
+       }
+    }
+}
+            
+       
+    
