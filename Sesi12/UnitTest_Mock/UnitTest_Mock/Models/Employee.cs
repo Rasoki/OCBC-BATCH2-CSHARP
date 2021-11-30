4c188e21498375222578ee9919f3248ab081f6ab
Rasoki <salas.rasoki@gmail.com>
2021-11-30 14:36:49 +0700

    tambah sesi 12

---
+++ Sesi12/UnitTest_Mock/UnitTest_Mock/Models/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace UnitTest_Mock.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Designation { get; set; }
+    }
+}
